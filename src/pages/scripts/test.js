2b26e251568c586edc85ab75be0014c67f5d7a8f
Ballademager <118718923+Ballademager@users.noreply.github.com>
2023-03-13 20:55:59 +0100

    test js added

---
+++ src/pages/scripts/test.js
@@ -0,0 +1,14 @@
+// https://awexnuldmrybuhripyxr.supabase.co
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF3ZXhudWxkbXJ5YnVocmlweXhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzg3MzEzODUsImV4cCI6MTk5NDMwNzM4NX0.oiwJey3e46ewy1-kC2D-dKF-l7ESxOX8UV7n1hLOizs
+fetch("https://awexnuldmrybuhripyxr.supabase.co/rest/v1/test_tabel", {
+  method: "GET",
+  headers: {
+    apikey: "eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF3ZXhudWxkbXJ5YnVocmlweXhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzg3MzEzODUsImV4cCI6MTk5NDMwNzM4NX0",
+  },
+})
+  .then((res) => res.json())
+  .then(showData);
+
+function showData(items) {
+  items.forEach();
+}
