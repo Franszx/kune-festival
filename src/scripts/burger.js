29aa277ea9aaea2415a4a9a14236a682194846ab
Ballademager <118718923+Ballademager@users.noreply.github.com>
2023-03-16 14:03:58 +0100

    ticket_button hidden

--- src/scripts/burger.js
+++ src/scripts/burger.js
@@ -1,6 +1,8 @@
 const menu_btn = document.querySelector(".burger");
 const nav_menu = document.querySelector("nav");
+const ticket_btn = document.querySelector(".buy_ticket_btn");
 menu_btn.addEventListener("click", function () {
   menu_btn.classList.toggle("is-active");
   nav_menu.classList.toggle("is-active");
+  ticket_btn.classList.toggle("hidden");
 });
